307679ab146e33ec9056517fc20b32ac0efcb617
XLongGitHub <1311620554@qq.com>
2016-11-19 22:14:50 +0800

    one

---
+++ 2016_11_19/Cal1_1_12.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Xulong on 2016/11/19.
+ */
+public class Cal1_1_12 {
+	public static void main(String args[]) {
+		int[] a = new int[10];
+
+		for (int i=0; i<10; i++) {
+			a[i] = 9 - i;
+		}
+
+		for (int i=0; i<10; i++) {
+			a[i] = a[a[i]];   //原数组中，部分数据被新生成的数据覆盖
+		}
+
+		for (int i=0; i<10; i++) {
+			System.out.println(a[i]);
+		}
+	}
+}
---
+++ 2016_11_19/Cal1_1_8.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Xulong on 2016/11/19.
+ */
+public class Cal1_1_8 {
+	public static void print(Object o) {
+		System.out.println(o);
+	}
+	
+	public static void main(String args []) {
+		print('b');
+		print('b' + 'c');
+        System.out.println('b' + 'c');
+		print((char)('a' + 4));
+	}
+
+}
